24b75098d516ce35112e2cbe1c6e6657400c7529
A-amirul <amirulislamamir412@gmail.com>
2024-04-10 12:57:10 +0600

    topbar is added

--- app/(root)/layout.js
+++ app/(root)/layout.js
@@ -1,6 +1,7 @@
 import { Inter } from "next/font/google";
 import "../globals.css";
 import Provider from "@components/Provider";
+import TopBar from "@components/Topbar";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -14,7 +15,8 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body className={inter.className}>
         <Provider>
-        {children}
+          <TopBar />
+          {children}
         </Provider>
       </body>
     </html>
